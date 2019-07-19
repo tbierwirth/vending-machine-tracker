fd7dd32d5cbc443a0001a19be37891090daa9845
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-19 09:22:06 -0600

    Add snack and machine_snack model and create associations

--- spec/models/snack_spec.rb
+++ spec/models/snack_spec.rb
@@ -4,7 +4,7 @@ RSpec.describe Snack, type: :model do
   describe 'validations' do
     it { should validate_presence_of :name }
     it { should validate_presence_of :price }
-    it {should have_many :machines_snacks}
-    it {should have_many(:machines).through(:machines_snacks)}
+    it {should have_many :machine_snacks}
+    it {should have_many(:machines).through(:machine_snacks)}
   end
 end
